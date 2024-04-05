934d4e6c25988d844812f2ed3a09d96a3f0408be
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-05 18:11:43 -0300

    refactor: apis url and keys

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,9 @@
 
-import axios from "axios";
+export const OPEN_WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
 
-export const api = axios.create({
-  baseURL: 'http://apiadvisor.climatempo.com.br',
-});
+export const OPEN_WEATHER_API_KEY = process.env.OPEN_WEATHER_API_KEY
+export const RAPID_API_KEY = process.env.RAPID_API_KEY
+
+export const GEO_DB_CITIES_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const RAPID_API_HOST = 'wft-geo-db.p.rapidapi.com'
