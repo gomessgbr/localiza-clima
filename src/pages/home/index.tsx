72bde51a6d34c49b2fd43ae8ba1cdd3627fec2e8
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-05 18:55:50 -0300

    refactor: home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,18 @@
+import { useEffect } from "react";
 import { FormComponent } from "../../components";
+import { Forecast } from "../../components/Forecast/Forecast";
+import { useGetForecast } from "../../components/ListCities/hooks/useGetForecast";
 
 import './styles.css'
 export function Home(){
+  // const {forecast} = useGetForecast()
+
   return (
     <div className="App">
       <div className="main-square">
-        <h1>Previsão do Tempo para </h1>
+        <h1>Localiza Clima</h1>
         <FormComponent title="Insira a Cidade"/>
+           {/* <Forecast  /> */}
       </div>
     </div>
   )
