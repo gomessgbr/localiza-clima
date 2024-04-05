e40362df8c916ff4ae90e4914b546c3d9331b11a
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-05 18:11:18 -0300

    feat: new env var

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,9 @@ export default defineConfig(({mode})=>{
   const env = loadEnv(mode, process.cwd(), '');
   return{
     define: {
-      'process.env.CLIMA_TEMPO_KEY': JSON.stringify(env.CLIMA_TEMPO_KEY)
+      'process.env.CLIMA_TEMPO_KEY': JSON.stringify(env.CLIMA_TEMPO_KEY),
+      'process.env.OPEN_WEATHER_API_KEY': JSON.stringify(env.OPEN_WEATHER_API_KEY),
+      'process.env.RAPID_API_KEY': JSON.stringify(env.RAPID_API_KEY),
     },
     plugins: [react()],
   }
