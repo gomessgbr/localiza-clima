3c84f76b3b6dde79a7143903991eb94389e0aca9
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-05 18:53:27 -0300

    feat: export files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,3 @@
 export * from './Form/Form';
+export * from './ListCities/ListCities';
+export * from './Forecast/Forecast'
---
+++ src/model/index.ts
@@ -0,0 +1,2 @@
+export * from './WeatherApiResponseModel';
+export * from './GeoDbApiModel';
