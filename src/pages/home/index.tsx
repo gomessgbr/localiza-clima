fe29bd47643a5fe79ab74c0349a8a24bfdf66f94
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-05 18:14:03 -0300

    feat: refactor home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,62 +1,12 @@
-import { useEffect, useState } from "react";
-import { fetchForecastForSP } from "../../services/ClimaTempoService";
-import { ForeCastResponseModel } from "../../model/ForeCastResponseModel";
+import { FormComponent } from "../../components";
 
 import './styles.css'
 export function Home(){
-  const [forecast, setForecast] = useState<ForeCastResponseModel | null>(null);
-  useEffect(() => {
-    fetch()
-  }, [])
-
-  const fetch = async () =>{
-    const response = await fetchForecastForSP();
-    setForecast(response)
-  }
-
-
   return (
     <div className="App">
       <div className="main-square">
-        <h1>Previsão do Tempo para {forecast?.name}</h1>
-        {/* <FormComponent title="Insira a Cidade"/> */}
-        <div>
-          
-          <p>Resumo</p>
-          <p>{forecast?.data[0].text_icon.text.pt}</p>
-        </div>
-        <div>
-          <p>Temperatura</p>
-          <p>Min: {forecast?.data[0].temperature.min}°</p>
-          <p>Max: {forecast?.data[0].temperature.max}°</p>
-        </div>
-        <div>
-          <p>Umidade</p>
-          <p>Min: {forecast?.data[0].humidity.min}%</p>
-          <p>Max: {forecast?.data[0].humidity.max}%</p>
-        </div>
-        <div>
-          <p>Previsão para os próximos 5 dias</p>
-          <div>
-          <p>{forecast?.data[0].date_br}</p>
-          <p>Min: {forecast?.data[0].temperature.min}°</p>
-          <p>Max: {forecast?.data[0].temperature.max}°</p>
-          </div>
-          <div>
-            <p></p>
-          </div>
-          <div>
-            <p></p>
-          </div>
-          <div>
-            <p></p>
-          </div>
-          <div>
-            <p></p>
-          </div>
-          
-
-        </div>
+        <h1>Previsão do Tempo para </h1>
+        <FormComponent title="Insira a Cidade"/>
       </div>
     </div>
   )
