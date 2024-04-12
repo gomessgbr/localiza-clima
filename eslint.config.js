5ecdb385bc7008cb91b034fd5894385eedc7e15e
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:03:29 -0300

    lint: eslint

---
+++ eslint.config.js
@@ -0,0 +1,17 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+export default [
+  {
+    languageOptions: { globals: globals.browser },
+
+    plugins:{
+      
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReactConfig,
+];
