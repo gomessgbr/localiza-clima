4da5ab4a4be042ca5976031c920ab04a374260f4
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:49:25 -0300

    refactor: home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,22 +1,14 @@
-import { Card, FormComponent } from "../../components";
+import {  FormComponent } from "../../components";
+import { useGetForecast } from "../../components/ListCities/hooks/useGetForecast";
 
 import "./styles.css";
 export function Home() {
-  // const {forecast} = useGetForecast()
+  const {forecast} = useGetForecast();
 
   return (
     <div className="main">
       <div className="main-square">
-        {/* <h1>Localiza Clima</h1> */}
-        {/* <FormComponent title="Insira a Cidade"/> */}
-        <Card
-          location="São Paulo"
-          currently="23"
-          icon="https://openweathermap.org/img/wn/01d@2x.png"
-          max="30"
-          min="20"
-          description="Partialmente nublado"
-        />
+        <FormComponent title="Insira a Cidade"/>
       </div>
     </div>
   );
