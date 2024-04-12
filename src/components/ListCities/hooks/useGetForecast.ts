0a7e606751832e0077d082ced4bf9602861c7ef1
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:49:15 -0300

    refactor: try catch to get forecast hook

--- src/components/ListCities/hooks/useGetForecast.ts
+++ src/components/ListCities/hooks/useGetForecast.ts
@@ -3,18 +3,23 @@ import { useState } from "react";
 import { OPEN_WEATHER_API_KEY, OPEN_WEATHER_API_URL } from "../../../api/api";
 import { ForecastAPIResponse } from "../../../model";
 
-
 export function useGetForecast() {
   const [forecast, setForecast] = useState<ForecastAPIResponse | null>(null);
-const getForecast = async(lat: number, lon: number) => {
-  const response = await fetch(`${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`)
-  const responseJson = await response.json();
-  setForecast(responseJson);
-  return
-}
+  const getForecast = async (lat: number, lon: number) => {
+    try {
+      const response = await fetch(
+        `${OPEN_WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${OPEN_WEATHER_API_KEY}&units=metric`
+      );
+      const responseJson = await response.json();
+      setForecast(responseJson);
+      return;
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-return {
-  getForecast,
-  forecast
-}
+  return {
+    getForecast,
+    forecast,
+  };
 }
