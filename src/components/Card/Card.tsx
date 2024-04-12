45e8002b92b59133621c909f14de36966578dabb
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 18:41:13 -0300

    feat: convert to int double weathers

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -9,16 +9,19 @@ interface ICardProps {
   description: string;
 }
 export function Card(ICardProps: ICardProps) {
+  const max = Math.floor(ICardProps.max);
+  const min = Math.floor(ICardProps.min);
+  const currently = Math.floor(ICardProps.currently);
   return (
     <div className="card-area">
       <div className="card-forecast-area">
         <h1>{ICardProps.location}</h1>
         <img src={ICardProps.icon} alt="icon" />
-        <p className="currently-forecast">{ICardProps.currently}°C</p>
-        <p> {ICardProps.description}</p>
+        <p className="currently-forecast">{currently}°C</p>
+        <p>{ICardProps.description}</p>
         <div className="max-min-area">
-          <p>Máx: {ICardProps.max}°</p>
-          <p>Mín: {ICardProps.min}°</p>
+          <p>Máx: {max}°</p>
+          <p>Mín: {min}°</p>
         </div>
       </div>
     </div>
