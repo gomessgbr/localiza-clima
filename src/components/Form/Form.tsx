a29f7a006ae7e32efdceaa64f73e2b8a112a3a4b
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:48:58 -0300

    refactor: form forecast

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -3,6 +3,9 @@ import { FormEvent, useId } from "react";
 import { useGetLocale } from "./hooks/useGetLocale";
 import { ListCities } from "../ListCities/ListCities";
 
+import { useGetForecast } from "../ListCities/hooks/useGetForecast";
+import { Card } from "../Card/Card";
+
 import "./styles.css";
 
 interface IFormProps {
@@ -11,14 +14,17 @@ interface IFormProps {
 export function FormComponent({ title }: IFormProps) {
   const id = useId();
   const { locale, getLocale } = useGetLocale();
+  const { forecast } = useGetForecast();
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
     const inputElement = event.currentTarget.elements[0] as HTMLInputElement;
-     await getLocale(inputElement.value);
+    await getLocale(inputElement.value);
     event.preventDefault();
   }
+
   return (
     <>
+      <h1>Localiza Clima</h1>
       <form id={id} className="form-area" onSubmit={handleSubmit}>
         <label id={id}>
           {title}
@@ -29,7 +35,16 @@ export function FormComponent({ title }: IFormProps) {
         </button>
       </form>
 
-      {locale && locale.data.length > 0 && <ListCities data={locale.data} />}
+      {locale && locale.data.length > 0 && !forecast && <ListCities data={locale.data} />}
+      {forecast &&
+      <Card
+          location={forecast.name}
+          currently={forecast.main.temp}
+          icon={`https://openweathermap.org/img/wn/${forecast.weather[0].icon}@4x.png`}
+          max={forecast.main.temp_max}
+          min={forecast.main.temp_min}
+          description={forecast.weather[0].description}
+        />}
     </>
   );
 }
