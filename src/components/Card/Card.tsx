1dcb90c4d4ff20890e517e3c96c346e350b4143c
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:24:22 -0300

    feat: card forecast component

---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,26 @@
+import "./styles.css";
+
+interface ICardProps {
+  location: string;
+  icon: string;
+  max: string;
+  min: string;
+  currently: string;
+  description: string;
+}
+export function Card(ICardProps: ICardProps) {
+  return (
+    <div className="card-area">
+      <div className="card-forecast-area">
+        <h1>{ICardProps.location}</h1>
+        <img src={ICardProps.icon} alt="icon" />
+        <p className="currently-forecast">{ICardProps.currently}°C</p>
+        <p> {ICardProps.description}</p>
+        <div className="max-min-area">
+          <p>Máx: {ICardProps.max}°</p>
+          <p>Mín: {ICardProps.min}°</p>
+        </div>
+      </div>
+    </div>
+  );
+}
