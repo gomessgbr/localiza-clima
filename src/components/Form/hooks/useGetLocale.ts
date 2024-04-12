9c7ee01ba4032050128bba7d54f265af13800d8a
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-04-12 17:03:57 -0300

    refactor: handle error call api

--- src/components/Form/hooks/useGetLocale.ts
+++ src/components/Form/hooks/useGetLocale.ts
@@ -1,29 +1,32 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { GEO_DB_CITIES_API_URL, RAPID_API_KEY } from "../../../api/api";
 import { GeoDBCityResponse } from "../../../model/GeoDbApiModel";
 
-
-
 export function useGetLocale() {
-
   const [locale, setLocale] = useState<GeoDBCityResponse | null>(null);
-  const getLocale = async (citieName : string) => {
-      const response = await fetch(`${GEO_DB_CITIES_API_URL}/cities?minPopulation=1000000&namePrefix=${citieName}`, {
-        headers: {
-          'X-RapidAPI-Key' : `${RAPID_API_KEY}`,
-          'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+  const getLocale = async (citieName: string) : Promise<string> => {
+    try {
+      const response = await fetch(
+        `${GEO_DB_CITIES_API_URL}/cities?minPopulation=1000000&namePrefix=${citieName}`,
+        {
+          headers: {
+            "X-RapidAPI-Key": `${RAPID_API_KEY}`,
+            "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+          },
         }
-      })
+      );
 
       const responseJson = await response.json();
       setLocale(responseJson);
-      return
-  }
-
-
+      return 'Sucesso';
+    } catch (e) {
+      console.log(e);
+      return 'Erro';
+    }
+  };
 
   return {
     locale,
-    getLocale
-  }
+    getLocale,
+  };
 }
